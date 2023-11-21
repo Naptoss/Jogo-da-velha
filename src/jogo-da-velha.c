2b2e8fb55d6c650dacb72c4690f96e2aad54eae0
Naptoss <tomgabinio@gmail.com>
2023-11-21 15:43:38 -0300

    subindo as variaveis

--- src/jogo-da-velha.c
+++ src/jogo-da-velha.c
@@ -3,7 +3,13 @@
 #include <time.h>
 #include <locale.h>
 
-// 
+char campo[10];
 
-
+void umJogador();           
+void doisJogadores();       
+void ajuda();               
+int checarCompleto();
+int checarVitoria(char);
+char entradaUsuario(int);
+void IA(int);
 
