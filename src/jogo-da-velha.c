0aae06ad29a10fb77b2811fa8eeff2f20a99491a
Naptoss <tomgabinio@gmail.com>
2023-11-21 16:22:54 -0300

    criada a logica do menu

--- src/jogo-da-velha.c
+++ src/jogo-da-velha.c
@@ -25,10 +25,33 @@ void resetarCampo() {
     };
 }
 
-void menu(){
-    while (1){
-        resetarCampo();
-        char entradaMenu;
-        printf("\n\n");
+void menu() {                  
+    while (1) {                 
+        resetarCampo();         
+        char entradaMenu;       
+        printf("\n\n"           
+               "MENU\n\n"
+               "1: Um Jogador\n"
+               "2: Dois Jogadores\n"
+               "3: Ajuda\n"
+               "4: Créditos\n"
+               "5: Sair\n\n\n"
+               "Entrada: ");
+        scanf(" %c", &entradaMenu);     
+        if (entradaMenu == '1')        
+            umJogador();
+        else if (entradaMenu == '2')    
+            doisJogadores();
+        else if (entradaMenu == '3')    
+            ajuda();
+        else if (entradaMenu == '4')
+            creditos();
+        else if (entradaMenu == '5' || entradaMenu == 's')  // Se a entrada for 4 ou s, o programa é encerrado.
+            return;
+        else {
+            limparConsole();
+            printf("\nEntrada Inválida!");   // Se a entrada for inválida, o usuário é informado e o menu é impresso novamente.
+        }
     }
 }
+
